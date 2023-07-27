bf05435a9c243f8c37824cb2c0e540220a832f96
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-07-27 15:16:07 -0600

    added: ResponseEntity<> AulaController

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/AulaController.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/AulaController.java
@@ -7,8 +7,11 @@ import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Pa
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.enumeradores.Pizarron;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.AulaDAO;
 import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.PabellonDAO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -36,36 +39,48 @@ public class AulaController extends GenericController<Aula, AulaDAO>{
     }
 
     @PutMapping("/{idAula}/pabellon/{idPabellon}")
-    public Aula asignarPabellonAula(@PathVariable Integer idAula, @PathVariable Integer idPabellon){
+    public ResponseEntity<?> asignarPabellonAula(@PathVariable Integer idAula, @PathVariable Integer idPabellon){
+        Map<String, Object> mensaje = new HashMap<>();
         Optional<Aula> oAula = service.findById(idAula);
         if(!oAula.isPresent()) {
-            throw new BadRequestException(String.format("Aula con id %d no existe", idAula));
+            //throw new BadRequestException(String.format("Aula con id %d no existe", idAula));
+            mensaje.put("succes", Boolean.FALSE);
+            mensaje.put("message", String.format("Aula con id %d no existe", idAula));
+            return ResponseEntity.badRequest().body(mensaje);
         }
         Optional<Pabellon> oPabellon = pabellonDAO.findById(idPabellon);
         if(!oPabellon.isPresent()){
-            throw new BadRequestException(String.format("Pabellon con id %d no existe", idPabellon));
+            //throw new BadRequestException(String.format("Pabellon con id %d no existe", idPabellon));
+            mensaje.put("succes", Boolean.FALSE);
+            mensaje.put("message", String.format("Alumno con id %d no existe", idPabellon));
+            return ResponseEntity.badRequest().body(mensaje);
         }
-
-        Pabellon pabellon = oPabellon.get();
         Aula aula = oAula.get();
-        aula.setPabellon(pabellon);
-        return service.save(aula);
+        Pabellon pabellon = oPabellon.get();
+        ((Aula)aula).setPabellon(pabellon);
+        mensaje.put("datos", service.save(aula));
+        mensaje.put("succes", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
     }
 
     @PutMapping("/{id}")
-    public Aula actualizarAula(@PathVariable Integer id, @RequestBody Aula aula){
-        Aula aulaUpdate = null;
+    public ResponseEntity<?> actualizarAula(@PathVariable Integer id, @RequestBody Aula aula){
+        Map<String, Object> mensaje = new HashMap<>();
         Optional<Aula> oAula = service.findById(id);
         if(!oAula.isPresent()) {
-            throw new BadRequestException(String.format("Aula con id %d no existe", id));
+            //throw new BadRequestException(String.format("Aula con id %d no existe", id));
+            mensaje.put("succes", Boolean.FALSE);
+            mensaje.put("message", String.format("Aula con id %d no existe", id));
+            return ResponseEntity.badRequest().body(mensaje);
         }
-        aulaUpdate =  oAula.get();
+        Aula aulaUpdate = oAula.get();
         aulaUpdate.setNroAula(aula.getNroAula());
         aulaUpdate.setMedidas(aula.getMedidas());
         aulaUpdate.setPizarron(aula.getPizarron());
         aulaUpdate.setCantidadPupitres(aula.getCantidadPupitres());
-
-        return service.save(aulaUpdate);
+        mensaje.put("datos", service.save(aulaUpdate));
+        mensaje.put("succes", Boolean.TRUE);
+        return ResponseEntity.ok(mensaje);
     }
 }
 
