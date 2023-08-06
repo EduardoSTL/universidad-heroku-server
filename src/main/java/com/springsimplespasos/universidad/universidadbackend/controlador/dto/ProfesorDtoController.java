12fcb1e07d989f98991dc310610bc5f235ff9618
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-08-05 19:22:59 -0600

    added: ProfesorDtoController

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/dto/ProfesorDtoController.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/controlador/dto/ProfesorDtoController.java
@@ -1,11 +1,196 @@
 package com.springsimplespasos.universidad.universidadbackend.controlador.dto;
 
+import com.springsimplespasos.universidad.universidadbackend.modelo.dto.PersonaDTO;
+import com.springsimplespasos.universidad.universidadbackend.modelo.dto.ProfesorDTO;
+import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Carrera;
+import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Persona;
+import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Profesor;
+import com.springsimplespasos.universidad.universidadbackend.modelo.mapper.mapstruck.CarreraMapperMS;
+import com.springsimplespasos.universidad.universidadbackend.modelo.mapper.mapstruck.ProfesorMapper;
+import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.CarreraDAO;
+import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.PersonaDAO;
+import com.springsimplespasos.universidad.universidadbackend.servicios.contratos.ProfesorDAO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/profesores")
 @ConditionalOnProperty(prefix = "app", name = "controller.enable-dto", havingValue = "true")
-public class ProfesorDtoController {
+public class ProfesorDtoController extends PersonaDtoController {
+    private final CarreraDAO carreraDAO;
+    private final CarreraMapperMS carreraMapperMs;
+
+    public ProfesorDtoController(@Qualifier("profesorDAOImpl") PersonaDAO service, ProfesorMapper profesorMapper, CarreraDAO carreraDAO, CarreraMapperMS carreraMapperMs) {
+        super(service, "profesor", profesorMapper);
+        this.carreraDAO = carreraDAO;
+        this.carreraMapperMs = carreraMapperMs;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllProfesores(){
+        Map<String, Object> mensaje = new HashMap<>();
+        List<PersonaDTO> dtos = super.findAll();
+        mensaje.put("succes", Boolean.TRUE);
+        mensaje.put("data", dtos);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findProfesorId(@PathVariable Integer id) {
+        Map<String, Object> mensaje = new HashMap<>();
+        PersonaDTO dto = super.findPersonaById(id);
+        if (dto == null) {
+            mensaje.put("succes", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No existe %s con Id %d", nombre_entidad, id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("succes", Boolean.TRUE);
+        mensaje.put("data", dto);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProfesor(@Valid @RequestBody PersonaDTO personaDTO, BindingResult result){
+        Map<String,Object> mensaje = new HashMap<>();
+        if (result.hasErrors()){
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("validaciones",super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        PersonaDTO save = super.createEntidadPersona(profesorMapper.mapProfesorDTO((ProfesorDTO) personaDTO));
+        mensaje.put("success",Boolean.TRUE);
+        mensaje.put("data",save);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mensaje);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProfesor(@PathVariable Integer id,
+                                            @Valid @RequestBody ProfesorDTO profesorDTO, BindingResult result){
+
+        Map<String,Object> mensaje = new HashMap<>();
+        PersonaDTO personaDTO= super.findPersonaById(id);
+        ProfesorDTO dto;
+        Profesor profesorUpdate;
+
+        if(personaDTO==null) {
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("mensaje",String.format("%s con id %d no existe",nombre_entidad, id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        if (result.hasErrors()){
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("validaciones",super.obtenerValidaciones(result));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        dto = ((ProfesorDTO)personaDTO);
+        dto.setNombre(profesorDTO.getNombre());
+        dto.setApellido(profesorDTO.getApellido());
+        dto.setDireccion(profesorDTO.getDireccion());
+        dto.setDni(profesorDTO.getDni());
+        dto.setSueldo(profesorDTO.getSueldo());
+
+        profesorUpdate = profesorMapper.mapProfesorDTO(dto);
+        mensaje.put("datos",super.createEntidadPersona(profesorUpdate));
+        mensaje.put("success",Boolean.TRUE);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProfesorId(@PathVariable Integer id){
+        Map<String,Object> mensaje = new HashMap<>();
+        PersonaDTO personaDTO = super.findPersonaById(id);
+        if(personaDTO==null) {
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje",  String.format("No existe %s con Id %d", nombre_entidad, id));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        super.deleteById(id);
+        mensaje.put("success",Boolean.TRUE);
+        return ResponseEntity.status(HttpStatus.OK).body(mensaje);
+    }
+
+    @GetMapping("/nombre-apellido/{nombre}/{apellido}")
+    public ResponseEntity<?> findProfesorNombreApellido(
+            @PathVariable String nombre, @PathVariable String apellido){
+        Map<String,Object> mensaje = new HashMap<>();
+        PersonaDTO personaDTO = super.findPersonaByNombreYApellido(
+                nombre,apellido);
+        if (personaDTO==null){
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("mensaje",String.format("No se encontro persona con nombre +%s y appelido %s",nombre,apellido));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos",personaDTO);
+        mensaje.put("success",Boolean.TRUE);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @GetMapping("/profesor-dni")
+    public ResponseEntity<Map<String, Object>> findProfesorDni(@RequestParam String dni){
+        Map<String,Object> mensaje = new HashMap<>();
+        PersonaDTO dto = super.findPersonaByDni(dni);
+        if (dto == null){
+            mensaje.put("success", Boolean.FALSE);
+            mensaje.put("mensaje", String.format("No se encontro %s con DNI: %s",nombre_entidad,dni));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        mensaje.put("datos",dto);
+        mensaje.put("success",Boolean.TRUE);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @GetMapping("/profesores-carreras")
+    public ResponseEntity<?> findProfesoresCarrera(@RequestBody String carrera){
+
+        Map<String,Object> mensaje = new HashMap<>();
+        List<Persona> personas = ((List<Persona>)((ProfesorDAO)service).buscarProfesoresPorCarrera(carrera));
+        List<ProfesorDTO> dtos = personas.stream()
+                .map(persona -> profesorMapper.mapProfesor((Profesor) persona))
+                .collect(Collectors.toList());
+
+        mensaje.put("success",Boolean.TRUE);
+        mensaje.put("data",dtos);
+        return ResponseEntity.ok().body(mensaje);
+    }
+
+    @PutMapping("/{idProfesor}/carrera/{idCarrera}")
+    public ResponseEntity<?> assignCarreraProfesor(@PathVariable Integer idProfesor, @PathVariable Integer idCarrera){
+
+        Map<String,Object> mensaje = new HashMap<>();
+        PersonaDTO personaDTO = super.findPersonaById(idProfesor);
+        Optional<Carrera> oCarrera;
+        Profesor profesor;
+        Carrera carrera;
+        Set<Carrera> carreras;
+
+        if(personaDTO==null) {
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("mensaje",String.format("%s con id %d no existe",nombre_entidad, idProfesor));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+
+        oCarrera = carreraDAO.findById(idCarrera);
+        if(oCarrera.isEmpty()){
+            mensaje.put("success",Boolean.FALSE);
+            mensaje.put("mensaje",String.format("Carrera con id %d no existe",idCarrera ));
+            return ResponseEntity.badRequest().body(mensaje);
+        }
+        profesor = profesorMapper.mapProfesorDTO((ProfesorDTO) personaDTO);
+        carrera = oCarrera.get();
+        carreras = new HashSet<>();
+        carreras.add(carrera);
+        profesor.setCarreras(carreras);
+
+        mensaje.put("success",Boolean.TRUE);
+        mensaje.put("data",service.save(profesor));
+        return ResponseEntity.ok().body(mensaje);
+    }
 }
